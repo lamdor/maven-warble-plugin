cd4f0309e39f88dd17e8f2d19426a79299f1c87c
Luke Amdor <luke@absoluterubbish.net>
2008-12-23 14:54:08 -0600

    Closing file input stream & using basedir invoker var

--- src/it/simple-rails/verify.groovy
+++ src/it/simple-rails/verify.groovy
@@ -1,6 +1,6 @@
 import org.jruby.plugins.WarFile
 
-WarFile warFile = new WarFile("src/it/simple-rails/tmp/simple-rails-1.0-SNAPSHOT.war")
+WarFile warFile = new WarFile(new File(basedir, "tmp/simple-rails-1.0-SNAPSHOT.war"))
 assert warFile.exists()
 assert warFile.contains("WEB-INF/web.xml")
 
--- src/test/java/org/jruby/plugins/WarFile.java
+++ src/test/java/org/jruby/plugins/WarFile.java
@@ -7,22 +7,36 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class WarFile {
-    private File warFile;
+    private File warFileLocation;
 
-    public WarFile(String warFileLocation) {
-        warFile = new File(warFileLocation);
+    public WarFile(File warFileLocation) {
+        this.warFileLocation = warFileLocation;
     }
 
     public boolean exists() {
-        return warFile.exists();
+        return warFileLocation.exists();
     }
 
-    public boolean contains(String entryName) throws IOException {
-        ZipInputStream zip = new ZipInputStream(new FileInputStream(warFile));
-        ZipEntry entry;
-        while ((entry = zip.getNextEntry()) != null) {
-            if (entry.getName().equals(entryName)) {
-                return true;
+    public boolean contains(String entryName) {
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(warFileLocation);
+            ZipInputStream zip = new ZipInputStream(inputStream);
+            ZipEntry entry;
+            while ((entry = zip.getNextEntry()) != null) {
+                if (entry.getName().equals(entryName)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
         return false;

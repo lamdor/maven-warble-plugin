27e3d838854b1eaf725bd2d98ed1d9b75c0bb9e2
Luke Amdor <luke@absoluterubbish.net>
2008-12-22 19:08:40 -0600

    Added packaging lifecycle

---
+++ src/main/java/org/jruby/plugins/WarbleMojo.java
@@ -0,0 +1,18 @@
+package org.jruby.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Uses Warble to change your Rails/Rack application into a WAR
+ *
+ * @goal warble
+ * @phase package
+ */
+public class WarbleMojo extends AbstractMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("I was running!!");
+    }
+}

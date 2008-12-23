7faafb95e67c01beb77e16bd5717336c17f4308d
Luke Amdor <luke@absoluterubbish.net>
2008-12-22 23:52:13 -0600

    Fixed verify to actually verify that we have a web.xml in our war

--- src/it/simple-rails/verify.groovy
+++ src/it/simple-rails/verify.groovy
@@ -1,13 +1,23 @@
 import java.util.jar.JarInputStream
+import java.util.zip.ZipInputStream
+import java.io.InputStream
+import java.util.zip.ZipEntry
 
 File warFile = new File("src/it/simple-rails/tmp/simple-rails-1.0-SNAPSHOT.war")
 assert warFile.exists()
 
-FileInputStream fileInputStream = new FileInputStream(warFile)
-JarInputStream jarInputStream = new JarInputStream(fileInputStream)
+FileInputStream war = new FileInputStream(warFile)
 
-Map warManifest = jarInputStream.getManifest().entries
-println "Manifest ${warManifest}"
-assert warManifest.containsKey("web.xml")
+boolean zipContains(InputStream inputSteam, String name) {
+    ZipInputStream zip = new ZipInputStream(inputSteam)
+    while ((entry = zip.nextEntry) != null) {
+        if (entry.name == name) {
+            return true
+        }
+    }
+    return false
+}
+
+assert zipContains(war, "WEB-INF/web.xml")
 
 return true
